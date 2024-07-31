7c73030aa9db6e606c1e8254371c8bf7580e33ce
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-07-31 17:05:23 +0100

    complet task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -34,6 +34,7 @@ def dipslay_text(text):
     return f"C {text}"
 
 
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text="is cool"):
     """
