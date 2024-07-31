8253ff3d8301b632c815c17013bde5b3341ae4ac
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-07-31 16:58:08 +0100

    complet task 1

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -23,7 +23,7 @@ def hbnb():
     return "HBNB"
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def dipslay_text(text):
     """
     Display "C" followed by the value of the
