03036a4e385ab093c520689a1211beebb30d8f7f
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-07-31 15:06:06 +0100

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that starts a Flask web application
+This Script starts a Flask web application.
 """
 from flask import Flask
 
@@ -11,10 +11,14 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     """
-    Displays Hello HBNB!
+    This function displays Hello HBNB!
     """
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=5000,
+        debug=True
+    )
