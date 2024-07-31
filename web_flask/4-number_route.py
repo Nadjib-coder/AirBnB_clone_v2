5d5b942dde7a9b8361f846fc536bd8dd1eb8038f
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-07-31 17:25:44 +0100

    complet task

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+a script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    a function that display Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    a function that display HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def dipslay_text(text):
+    """
+    Display "C" followed by the value of the
+    variable 'txt' woth underscores replaced
+    by spaces
+    """
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """
+    Display "Python" followed by the value
+    of the text variable
+    """
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('number/<int:n>', strict_slashes=False)
+def python_n(n):
+    """
+    a function that display "n is a number" only if
+    n is an integer
+    """
+    if isinstance(n, int):
+        return f'{n} is a number'
+
+
+if __name__ == '__main__':
+    app.run(
+            host='0.0.0.0',
+            port=5000,
+            debug=True
+            )
